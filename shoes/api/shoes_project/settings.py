4ce1c817276a67d5fe122ae5ced2ff36c23bcb6d
Alex Porter <alpo1992@gmail.com.here>
2022-07-27 18:33:30 -0600

    added the models, registered in admin, made a get,post view for shoes list

--- shoes/api/shoes_project/settings.py
+++ shoes/api/shoes_project/settings.py
@@ -31,7 +31,9 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+
 INSTALLED_APPS = [
+    "shoes_rest.apps.ShoesApiConfig",
     "corsheaders",
     'django.contrib.admin',
     'django.contrib.auth',
